5acaed0defd324472a4049987a68b71a1f65a052
ww456zz <ww456zzdd@gmail.com>
2022-07-24 20:44:36 -0500

    3rd

--- scripts.js
+++ scripts.js
@@ -1,14 +1,18 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
-const flightStatus = document.getElementById("flightStatus");
-const background = document.getElementById("shuttleBackground");
-const takeoff = document.getElementById("takeoff");
+
 function init () {
+    const flightStatus = document.getElementById("flightStatus");
+    const shuttleBackground = document.getElementById("shuttleBackground");
+    const takeoff = document.getElementById("takeoff");
+
+
     takeoff.addEventListener('click', event => {
         let x = window.confirm("Confirm that the shuttle is ready for takeoff.");
         if (x) {
+            shuttleBackground.style.backgroundColor = "blue";
             flightStatus.innerHTML = "Shuttle in flight.";
-            event.target.style.backgroundColor = "blue"
+            document.getElementById("spaceShuttleHeight").innerHTML = 10000
 
             }
     })
